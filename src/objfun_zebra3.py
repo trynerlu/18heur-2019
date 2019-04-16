b491237551d2350e0dab5bc15401986a90cdbdf6
Matej Mojzes <matej.mojzes@jumpshot.com>
2019-04-16 11:22:37 +0200

    Eight class - Genetic Optimization and Clerc's Zebra-3 objective function

---
+++ src/objfun_zebra3.py
@@ -0,0 +1,50 @@
+from objfun import ObjFun
+import numpy as np
+
+
+class Zebra3(ObjFun):
+
+    def __init__(self, d):
+        self.fstar = 0
+        self.d = d
+        self.n = d*3
+        self.a = np.zeros(self.n, dtype=int)
+        self.b = np.ones(self.n, dtype=int)
+
+    def generate_point(self):
+        return np.random.randint(0, 1+1, self.n)
+
+    def get_neighborhood(self, x, d):
+        assert d == 1, "Zebra3 supports neighbourhood with (Hamming) distance = 1 only"
+        nd = []
+        for i, xi in enumerate(x):
+            xx = x.copy()
+            xx[i] = 0 if xi == 1 else 1
+            nd.append(xx)
+        return nd
+
+    def evaluate(self, x):
+        f = 0
+        for i in np.arange(1, self.d+1):
+            xr = x[(i-1)*3:i*3]
+            s = np.sum(xr)
+            if np.mod(i,2) == 0:
+                if s == 0:
+                    f += 0.9
+                elif s == 1:
+                    f += 0.6
+                elif s == 2:
+                    f += 0.3
+                else:  # s == 3
+                    f += 1.0
+            else:
+                if s == 0:
+                    f += 1.0
+                elif s == 1:
+                    f += 0.3
+                elif s == 2:
+                    f += 0.6
+                else:  # s == 3
+                    f += 0.9
+        f = self.n/3-f
+        return f
